aa8a3ed80db9690b8261dfa8bb6a30e32f5d13ff
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-07-20 22:05:17 +0530

    add

---
+++ functions.js
@@ -0,0 +1,40 @@
+function main(){
+    //your code here
+    movement();
+    
+ }
+ 
+ function movement()
+ {
+    move();
+    move();
+    move();
+    move();
+    turnLeft();
+    move();
+    move();
+    move();
+    move();
+ }
+
+ //for digonal movement of kernal
+ function main(){
+    //your code here
+    putBeeper();
+    movement();
+    movement();
+    movement();
+    movement();
+ }
+ 
+ function movement()
+ {
+    
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+ }
+ 
+ 
