6a5d8c3fe91df947f969c0feb7402eaf166c5c7b
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-07-20 15:28:26 +0530

    add

--- slicing.js
+++ slicing.js
@@ -1,3 +1,8 @@
 var tweet = prompt("Compose your tweet");
 var tweett = tweet.slice(0,30);
 alert(tweett);
+
+var tweet = prompt("Compose your tweet");
+alert(tweet.slice(0,30));
+
+alert(prompt("Compose your tweet").slice(0,40));
