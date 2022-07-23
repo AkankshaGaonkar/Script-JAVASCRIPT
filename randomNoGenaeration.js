064874b7e136e869c8b952d03de345db11e63982
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-07-23 09:22:40 +0530

    add

--- randomNoGenaeration.js
+++ randomNoGenaeration.js
@@ -1,3 +1,11 @@
 var n = Math.random();
 n = n*6;
-n = Math.floor(n);
+n = Math.floor(n)+1;
+console.log(n);
+
+
+prompt("Type the 1st person name");
+prompt("Type the 2nd person name");
+var lovescore = Math.random()*100;
+lovescore = Math.floor(lovescore)+1;
+alert("Your love % is "+lovescore);
