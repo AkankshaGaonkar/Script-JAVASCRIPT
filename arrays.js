912757f8a8d6134b3dcde75958ff14bd7455e8d8
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-07-23 21:50:04 +0530

    add

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var people =["anu","ashu","divya","deepak","rony"];
+var namee = prompt("Type the person name");
+if(people.includes(namee))
+{
+    alert("Welcome to the party");
+}
+else{
+    alert("No entry");
+}
