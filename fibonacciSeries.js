a0450ce54c87f28e627ca64ed4c6786e5d590e40
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-07-24 11:32:16 +0530

    add

---
+++ fibonacciSeries.js
@@ -0,0 +1,23 @@
+function fibonacciGenerator(n) {
+    var arr = [];
+    if(n===1)
+    {
+    arr=[0];
+    }
+else if(n===2)
+    {
+    arr=[0,1];
+    }
+else
+{
+    arr=[0,1];
+    for(var i=2;i<n;i++)
+    {
+    arr.push(arr[arr.length-2]+arr[arr.length-1]);
+    }
+}
+return arr;
+}
+
+arr =fibonacciGenerator(6); 
+console.log(arr);
